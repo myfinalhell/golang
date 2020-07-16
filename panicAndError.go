f38621db6d53218bab22608da0d9ec06b334799f
shaolili <shaolili@fulu.com>
2020-07-16 21:32:00 +0800

    asdf

---
+++ panicAndError.go
@@ -0,0 +1,25 @@
+ipackage golang2
+
+import (
+	"log"
+	"regexp"
+	"runtime"
+)
+
+//定义一个捕获panic并且输出栈信息的函数
+func MyPanic() {
+	pan := recover()
+	if pan != nil {
+		var buf [4096]byte
+		n := runtime.Stack(buf[:], false)
+		//获取栈的string
+		stack := string(buf[:n])
+		//屏蔽panic.go和此go文件的stack
+		re := regexp.MustCompile(`(?i).*panicAndError\.go.*|.*panic\.go.*|.*MyPanic.*|panic\(.*`)
+		stack = re.ReplaceAllString(stack, "")
+		re2 := regexp.MustCompile(`(?m)^\s*$`)
+		stack = re2.ReplaceAllString(stack, "######")
+		log.Printf("%s\n%s", pan, stack)
+	}
+}
+
